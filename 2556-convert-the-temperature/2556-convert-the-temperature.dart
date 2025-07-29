c5de30a259448c122e51010c0dee6d5d5fb99d3c
jabeelroshanP <jabeelroshan500@gmail.com>
2025-07-29 09:24:44 +0530

    Time: 0 ms (100%), Space: 148.5 MB (78.57%) - LeetHub

--- 2556-convert-the-temperature/2556-convert-the-temperature.dart
+++ 2556-convert-the-temperature/2556-convert-the-temperature.dart
@@ -1,6 +1,5 @@
 class Solution {
   List<double> convertTemperature(double celsius) {
-          return <double>[celsius + 273.15, celsius * 1.80 + 32.00];
-
+      return <double>[celsius + 273.15, celsius * 1.80 + 32.00];
   }
 }
