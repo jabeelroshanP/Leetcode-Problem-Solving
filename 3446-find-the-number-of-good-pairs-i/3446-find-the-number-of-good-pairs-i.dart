7e35486a17d4cee0826bc14be3d3f6c2bc04fc1f
jabeelroshanP <jabeelroshan500@gmail.com>
2025-08-18 15:31:51 +0530

    Time: 2 ms (0%), Space: 151.5 MB (0%) - LeetHub

---
+++ 3446-find-the-number-of-good-pairs-i/3446-find-the-number-of-good-pairs-i.dart
@@ -0,0 +1,15 @@
+class Solution {
+  int numberOfPairs(List<int> nums1, List<int> nums2, int k) {
+
+    int count = 0;
+
+    for(var num1 in nums1){
+        for(var num2 in nums2){
+            if(num1 % (num2 * k) == 0){
+                count ++;
+            }
+        }
+    }
+    return count ;
+  }
+}
